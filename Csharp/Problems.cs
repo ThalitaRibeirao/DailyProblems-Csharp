f6c3c500c0f3e4a5e1c3bf019525c0012d424a2b
ThalitaRibeirao <thaly.alvessilva@gmail.com>
2024-01-14 13:49:45 -0300

    day 17: solve game duration (part 2) problem

--- Csharp/Problems.cs
+++ Csharp/Problems.cs
@@ -6,7 +6,7 @@ CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
 
 // Solve the specified problem
-new Problem16().Solve();
+new Problem17().Solve();
 
 Console.WriteLine("\n\nPress any key to kit");
 Console.ReadKey();
---
+++ Csharp/Problems/17.cs
@@ -0,0 +1,26 @@
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1047
+public class Problem17
+{
+	public void Solve()
+	{
+		string[] input = Console.ReadLine().Split(' ');
+
+		int initialHour = int.Parse(input[0]);
+		int initialMinute = int.Parse(input[1]);
+
+		int endHour = int.Parse(input[2]);
+		int endMinute = int.Parse(input[3]);
+
+        if (endHour < initialHour || (endHour == initialHour && endMinute <= initialMinute)) endHour += 24;
+        if (endMinute < initialMinute)
+        {
+            endMinute += 60;
+            endHour--;
+        }
+
+        int durationHour = endHour - initialHour;
+        int durationMinute = endMinute - initialMinute;
+
+        Console.WriteLine($"O JOGO DUROU {durationHour} HORA(S) E {durationMinute} MINUTO(S)");
+    }
+}
