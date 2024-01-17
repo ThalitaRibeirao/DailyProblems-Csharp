afe7241f6a23cf0724e4fa4d9e17c020446cced4
ThalitaRibeirao <thaly.alvessilva@gmail.com>
2024-01-16 23:50:36 -0300

    day 19: solve animal problem

--- Csharp/Problems.cs
+++ Csharp/Problems.cs
@@ -8,7 +8,7 @@ CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
 
 // Solve the specified problem
-new Problem18().Solve();
+new Problem19().Solve();
 
 Console.WriteLine("\n\nPress any key to kit");
 Console.ReadKey();
---
+++ Csharp/Problems/19.cs
@@ -0,0 +1,59 @@
+﻿// https://www.beecrowd.com.br/judge/pt/problems/view/1049
+
+public class Dictionary3D : Dictionary<string, Dictionary<string, Dictionary<string, string>>>{ }
+public class Dictionary2D : Dictionary<string, Dictionary<string, string>> { }
+public class Dictionary1D : Dictionary<string, string> { }
+
+public class Problem19
+{
+    public void Solve()
+    {
+        Dictionary3D animals = new Dictionary3D
+        {
+            { "vertebrado", new Dictionary2D
+                {
+                    {
+                        "ave", new Dictionary1D
+                        {
+                            { "carnivoro", "aguia" },
+                            { "onivoro", "pomba" }
+                        }
+                    },
+                    {
+                        "mamifero", new Dictionary1D
+                        {
+                            { "onivoro", "homem" },
+                            { "herbivoro", "vaca" }
+                        }
+                    }
+
+                }
+            },
+            { "invertebrado", new Dictionary2D
+                {
+                    {
+                        "inseto", new Dictionary1D
+                        {
+                            { "hematofago", "pulga" },
+                            { "herbivoro", "lagarta" }
+                        }
+                    },
+                    {
+                        "anelideo", new Dictionary1D
+                        {
+                            { "hematofago", "sanguessuga" },
+                            { "onivoro", "minhoca" }
+                        }
+                    }
+                }
+            }
+        };
+
+        string typeSkeleton = Console.ReadLine();
+        string typeAnimal = Console.ReadLine();
+        string typeFood = Console.ReadLine();
+
+        string animal = animals[typeSkeleton][typeAnimal][typeFood];
+        Console.WriteLine(animal);
+    }
+}
