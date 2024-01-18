0067720df215084b0c06012d0036f2501977bb12
ThalitaRibeirao <thaly.alvessilva@gmail.com>
2024-01-18 19:07:49 -0300

    refact: organizing files

---
+++ Problems.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+
+// Define the default culture as CultureInfo.InvariantCulture
+CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
+CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+
+// Solve the specified problem
+new Problem21().Solve();
+
+Console.WriteLine("\n\nPress any key to kit");
+Console.ReadKey();
---
+++ Problems/21.cs
@@ -0,0 +1,30 @@
+﻿//https://www.beecrowd.com.br/judge/pt/problems/view/1051
+using System.Globalization;
+using System.Collections.Generic;
+
+public class Problem21
+{
+	public void Solve()
+	{
+		string input = Console.ReadLine()!;
+		double wage = double.Parse(input, CultureInfo.InvariantCulture);
+        double fee = 0;
+        Dictionary<double[], double> fees = new Dictionary<double[], double>();
+        {
+            { [0, 2000], 0 },
+            { [2000, 3000], 0.08 },
+            { [3000, 4500], 0.18 },
+            { [4500, Double.MaxValue], 0.28 }
+        };
+
+        foreach (var f in fees)
+        {
+            if (wage <= f.Key[0]) break;
+            double baseValue = Math.Min(wage, f.Key[1]) - f.Key[0];
+            fee += baseValue * f.Value;
+        }
+
+        if (fee == 0) Console.WriteLine("Isento");
+        else Console.WriteLine($"R$ {fee.ToString("F2")}");
+    }
+}
