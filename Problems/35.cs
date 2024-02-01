c0a20fb7a10cbf92585496080fe71c2328adb98b
ThalitaRibeirao <thaly.alvessilva@gmail.com>
2024-02-01 15:05:33 -0300

    day 35: solve remainder of division equals 2 problem

---
+++ Problems/35.cs
@@ -0,0 +1,16 @@
+﻿//
+public class Problem35
+{
+    public void Solve()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 1000; i++)
+        {
+            if (i % n == 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
