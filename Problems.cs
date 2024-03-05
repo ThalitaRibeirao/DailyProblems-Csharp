24e3e57fbe4758b01ec7944f756ed59f96fef766
ThalitaRibeirao <thaly.alvessilva@gmail.com>
2024-03-05 19:12:16 -0300

    day 60: solve increasing sequence problem

--- Problems.cs
+++ Problems.cs
@@ -7,7 +7,7 @@ CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
 
 // Solve the specified problem
-new Problem59().Solve();
+new Problem60().Solve();
 
 // Quit
 Console.WriteLine("\nType any key to quit...");
---
+++ Problems/60.cs
@@ -0,0 +1,18 @@
+﻿//https://judge.beecrowd.com/pt/problems/view/1146
+public class Problem60
+{
+    public void Solve()
+    {
+        while (true)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if (x == 0) break;
+            else
+            {
+                IEnumerable<string> list = Enumerable.Range(1, x).Select(num => num.ToString());
+                string output = String.Join(" ", list.ToArray());
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
