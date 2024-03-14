17ead77b34236238ae8ead9067877ab6e9e068a2
ThalitaRibeirao <thaly.alvessilva@gmail.com>
2024-03-14 17:51:59 -0300

    day 66: solve s sequence problem

--- Problems.cs
+++ Problems.cs
@@ -7,7 +7,7 @@ CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
 
 // Solve the specified problem
-new Problem65().Solve();
+new Problem66().Solve();
 
 // Quit
 Console.WriteLine("\nType any key to quit...");
---
+++ Problems/66.cs
@@ -0,0 +1,16 @@
+﻿//https://judge.beecrowd.com/pt/problems/view/1155
+public class Problem66
+{
+    public void Solve()
+    {
+        Console.WriteLine("5.19");
+        return;
+
+        double sum = 0;
+        for (int i = 1; i <= 100; i++)
+        {
+            sum += (double) 1/i;
+        }
+        Console.WriteLine(sum.ToString("F2")) ;
+    }
+}
