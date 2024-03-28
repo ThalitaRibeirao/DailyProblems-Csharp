223d30409d1ec4b7c19bf73b387465103bff8714
ThalitaRibeirao <thaly.alvessilva@gmail.com>
2024-03-28 06:12:54 -0300

    day 78: solve fibonacci vector problem

---
+++ Problems/78.cs
@@ -0,0 +1,37 @@
+﻿//https://judge.beecrowd.com/pt/problems/view/1176
+
+public class Problem78
+{
+    public double[] GetFibonacci(double[] fibonacci, int length, int x)
+    {
+        for (int i = length; i <= x; i++)
+        {
+            fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+        }
+        return fibonacci;
+    }
+
+    public void Solve()
+    {
+        double[] fibonacci = new double[61];
+
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        int length = 2;
+
+        int n = int.Parse(Console.ReadLine()); 
+
+        for (int i = 0; i < n; i++) { 
+            int x = int.Parse((Console.ReadLine()));
+
+            if (x > length)
+            {
+                fibonacci = GetFibonacci(fibonacci, length,  x);
+                length = x + 1;
+            }
+            Console.WriteLine($"Fib({x}) = {fibonacci[x].ToString("F0")}");
+        }
+    }
+}
+
--- obj/Debug/net8.0/Problems.AssemblyInfo.cs
+++ obj/Debug/net8.0/Problems.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Problems")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f114464c631621d93db43c25d66981f028f1a5d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0532c4c17da653949f4e258cfcf68dd11d3d720")]
 [assembly: System.Reflection.AssemblyProductAttribute("Problems")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Problems")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
